d413571e27567f082e1acf1e446dc9da3c3bad15
jiangj4 <jjiaying.ing@gmail.com>
2015-11-21 08:01:19 -0800

    common.php

--- common.php
+++ common.php
@@ -2,6 +2,30 @@
 	common header and footer
 	
 	-->
+<?php
+	function newPDO(){
+		$db = new PDO("mysql:dbname=scheduleme;host=localhost;charset=utf8", "root","");
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		return $db;
+	}
+
+	function findID($username, $db){
+		$usernamequote = $db->quote($username);
+	
+		$query = "SELECT name
+				  FROM Users
+				  WHERE facebookid = $usernamequote
+				  LIMIT 1";
+		$rows = $db->query($query);
+
+		if($rows->rowCount() > 0){
+			// the actor is in the database, return its ID
+			return true;
+		}
+		return false;
+	}
+?>
 <html>
 	<head>
 	
